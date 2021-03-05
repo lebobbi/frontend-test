98430dbe280cdfe0bd72c7b5858fd960b774c47b
lebobbi <carlosalbertoballesteros@hotmail.com>
2021-03-04 20:15:52 -0700

    Add Styles for Home different sections

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Styled from "styled-components";
 
-const Page = Styled.div`
-  margin: 36px 64px;
+const Header = Styled.header`
+  margin: 36px 64px 36px;
 `;
-
-const Header = Styled.header``;
 
 const Title = Styled.h1`
   color: #333333;
@@ -27,18 +25,62 @@ const Description = Styled.p`
   text-align: left;
 `;
 
+const SearchSection = Styled.section``;
+
+const FilterNav = Styled.header`
+  border: 1px solid #e6e6e6;
+  border-left: none;
+  border-right: none;
+  box-sizing: border-box;
+  height: 80px;
+  width: 100%;
+`;
+
+const SearchResults = Styled.main`
+  margin: 36px 64px 242px ;
+`;
+
+const ResultsTitle = Styled.h2`
+  color: #333333;
+  font-size: 34px;
+  margin: 42px 0 64px;
+  letter-spacing: 1px;
+  line-height: 40px;
+`;
+
+const RestaurantList = Styled.ul`
+  display: flex;
+`;
+const RestaurantItem = Styled.li`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 80px;
+  padding: 0;
+  width: 304px;
+`;
+
 /** Mocks */
 const title = "Restaurants";
 const description =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+const resultsTitle = "All Restaurants";
 
 const Home = () => (
-  <Page>
+  <>
     <Header>
       <Title>{title}</Title>
       <Description>{description}</Description>
     </Header>
-  </Page>
+    <SearchSection>
+      <FilterNav />
+      <SearchResults>
+        <ResultsTitle>{resultsTitle}</ResultsTitle>
+        <RestaurantList>
+          <RestaurantItem />
+        </RestaurantList>
+      </SearchResults>
+    </SearchSection>
+  </>
 );
 
 export default Home;

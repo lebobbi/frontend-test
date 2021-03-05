f8d982e95a27fe377bb57200287bb960b62e2b91
lebobbi <carlosalbertoballesteros@hotmail.com>
2021-03-05 08:22:26 -0700

    Add mock information for status cuisine and price to restaurants

--- src/Components/Home/RestaurantList.jsx
+++ src/Components/Home/RestaurantList.jsx
@@ -18,18 +18,49 @@ const RestaurantList = ({ restaurants }) => (
 
 RestaurantList.defaultProps = {
   restaurants: [
-    { id: 1, name: "Restaurant 1" },
+    { id: 1, name: "Restaurant 1", cuisine: "Hamburguer", status: false },
     {
       id: 2,
+      price: "$$",
       name: "Very Long Name For A Restaurant 2 Test Overflow Alignment",
     },
-    { id: 3, name: "Restaurant 3" },
-    { id: 4, name: "Restaurant 4" },
-    { id: 5, name: "Restaurant 5" },
-    { id: 6, name: "Restaurant 6" },
-    { id: 7, name: "Restaurant 7" },
-    { id: 8, name: "Restaurant 8" },
-    { id: 9, name: "Restaurant 9" },
+    { id: 3, name: "Restaurant 3", status: true, cuisine: "thai", price: "$" },
+    {
+      id: 4,
+      name: "Restaurant 4",
+      status: true,
+      cuisine: "Mexican",
+      price: "$$",
+    },
+    {
+      id: 5,
+      name: "Restaurant 5",
+      status: false,
+      cuisine: "Japanese",
+      price: "$$$$",
+    },
+    {
+      id: 6,
+      name: "Restaurant 6",
+      status: false,
+      cuisine: "Chinese",
+      price: "$$",
+    },
+    {
+      id: 7,
+      name: "Restaurant 7",
+      status: true,
+      cuisine: "Mexican",
+      price: "$$$",
+    },
+    { id: 8, name: "Restaurant 8", status: true, cuisine: "Pizza", price: "$" },
+    {
+      id: 9,
+      name: "Restaurant 9",
+      status: true,
+      cuisine: "Italian",
+      price: "$$$",
+    },
   ],
 };
 

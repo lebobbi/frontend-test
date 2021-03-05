6b6f028c61e7faa1a7bc4d71a26eaabd074576fe
lebobbi <carlosalbertoballesteros@hotmail.com>
2021-03-04 19:22:28 -0700

    Add app routing nav through reach-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React, { memo } from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
+import { Home, Details } from "~/src/Components/Pages";
 
-const App = memo(() => <div>The app</div>);
+const App = memo(() => (
+  <Router>
+    <Home path="/" />
+    <Details path="/details/:restaurantId" />
+  </Router>
+));
 
 render(<App />, document.getElementById("root"));

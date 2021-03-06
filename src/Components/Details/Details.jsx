65e0ec42975cbccaf1875fe8a88207358f53fc74
lebobbi <carlosalbertoballesteros@hotmail.com>
2021-03-05 17:31:42 -0700

    Initial Outline of Details page

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Styled from "styled-components";
+import Header from "./Header";
 
-const Details = ({ restaurantId }) => <div>Details of {restaurantId}</div>;
+const MapSection = Styled.section``;
+const ReviewSection = Styled.section``;
+
+const Details = ({ restaurantId }) => (
+  <>
+    <Header />
+    <MapSection />
+    <ReviewSection />
+  </>
+);
 
 export default Details;

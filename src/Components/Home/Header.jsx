9c223323ae4fee9714fffd0d26d8f937672ffb81
lebobbi <carlosalbertoballesteros@hotmail.com>
2021-03-05 17:30:39 -0700

    Fix names of Header component for consistency and clarity

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Styled from "styled-components";
 import { Title } from "~/src/Components/Common";
 
-const Header = Styled.header`
+const Wrapper = Styled.header`
   margin: 36px 64px 36px;
 `;
 
@@ -12,27 +12,27 @@ const Description = Styled.p`
   font-size: 22px;
   line-height: 32px;
   letter-spacing: 0.9166666865348816px;
-  margin: 0;
+  margin: 8px 0 0;
   max-width: 752px;
   text-align: left;
 `;
 
-const Home = ({ title, description }) => (
-  <Header>
+const Header = ({ title, description }) => (
+  <Wrapper>
     <Title>{title}</Title>
     <Description>{description}</Description>
-  </Header>
+  </Wrapper>
 );
 
-Home.propTypes = {
+Header.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
 };
 
-Home.defaultProps = {
+Header.defaultProps = {
   title: "Restaurants",
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 };
 
-export default Home;
+export default Header;
